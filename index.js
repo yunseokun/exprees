f8ccdb1592278feb8888aed30994d8b0cfc07421
yunseokun <yunseokun789@gmail.com>
2021-06-17 19:54:07 +0900

    dk

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 let express = require('express');
 let app = express();
 
+app.set('view engine', 'ejs');
+
+app.use(express.json())
+
+app.use(express.urlencoded())
+
 app.get('/', (req, res) => {
-    res.send(`Hello World!`);
+    res.render(`index`);
 });
+
+app.get('/test', (req, res) => {
+    res.render(`test`);
+});
+
+app.post('/submit', (req, res) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
 
 app.listen(3000, () => {
     console.log("서버 열림!");
